7239df38c48f30c7f589b7fc96ae70932c111b5e
Florian Wurth <wurth.florian@gmail.com>
2017-08-29 22:12:29 +0200

    touch input wrapper

---
+++ Input/TouchInputWrapper.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class TouchInputWrapper 
+{
+	public static Vector2 GetTouchPosition( int _touch )
+	{
+		#if UNITY_EDITOR
+			return Input.mousePosition;
+		#else
+			return Input.touches[_touch].position;
+		#endif
+	}
+
+	public static bool GetTouchDown( int _touch )
+	{
+		#if UNITY_EDITOR
+			return Input.GetMouseButtonDown(_touch);
+		#else
+			return Input.touches[_touch].phase == TouchPhase.Begin ;
+		#endif
+
+	}
+
+	public static bool GetTouch(int _touch)
+	{
+		#if UNITY_EDITOR
+			return Input.GetMouseButton(_touch);
+		#else
+			return Input.touchCount-1 >= _touch;
+		#endif
+
+	}
+
+	public static int GetTouchCount()
+	{
+		#if UNITY_EDITOR
+			return 0;
+		#else
+			return Input.touchCount;
+		#endif
+
+	}
+		
+	
+}
